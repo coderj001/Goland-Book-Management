380d12e54dacf5007d5565e8092e935c6da9eaf0
coderj001 <amirajubolchi001@gmail.com>
2022-02-10 02:59:17 +0530

    addded create, delete and fetch api point

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/coderj001/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+
+const port string = "localhost"
+const host string = ":3000"
+
+func main()  {
+  r := mux.NewRouter()
+  routes.RegisterBookStoreRoutes(r)
+  http.Handle("/", r)
+  log.Fatal(http.ListenAndServe(host+port, r))
+}
--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,16 +1,63 @@
 package controllers
 
-func CreateBook() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/coderj001/go-bookstore/pkg/models"
+	"github.com/coderj001/go-bookstore/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	createBook := &models.Book{}
+	utils.ParseBody(r, createBook)
+	b := createBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
-func GetBook() {
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBook := models.GetAllBooks()
+	res, _ := json.Marshal(newBook)
+	w.Header().Set("Content-Type", "applcation/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
-func GetBookId() {
+func GetBookId(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "applcation/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
-func UpdateBook() {
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+  
 }
 
-func DeleteBook() {
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "applcation/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
